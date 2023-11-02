465c1037f6b54ac0e9347c4752dbb8eddf232508
Abhyanand <86824669+OneFriendlyCoder@users.noreply.github.com>
2023-11-02 22:12:30 +0530

    Ai text summarizer react app

---
+++ text_summarizer/src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc h-10'>
+      Transforming lengthy documents into concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
